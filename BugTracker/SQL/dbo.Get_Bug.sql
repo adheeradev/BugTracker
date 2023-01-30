a292a70915b6379e6cb8624b279ec407199f04eb
adheeradev <pushmurali@gmail.com>
2023-01-30 10:21:46 +0000

    Updated read me file and additional changes
    I have updated read me file and also refactored couple of classes and a store proc.

--- BugTracker/SQL/dbo.Get_Bug.sql
+++ BugTracker/SQL/dbo.Get_Bug.sql
@@ -6,13 +6,6 @@ AS
 				   b.Title,
 				   b.[Description],
 				   b.OpenedDate,
-				   b.StatusId,
-				   wfs.[Status],
-				   usr.[Name] AS AssignedToUserName
+				   b.StatusId
 			FROM [Bug] b
-			LEFT OUTER JOIN [WorkFlowStatus] wfs
-			ON b.StatusId = wfs.Id
-			LEFT OUTER JOIN [User] usr
-			ON b.AssignedToUserId = usr.Id
 			WHERE b.Id = @BugId
-GO
